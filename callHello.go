5239cf5ccb78b07508908e0f0d2901e99c67a9d1
rchaser53 <tayoshizawa29@gmail.com>
2017-12-02 11:44:33 +0900

    try to http get

--- callHello.go
+++ callHello.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	f "fmt"
-
 	nyanHello "goPlayground/nyanHello"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func main() {
-	f.Println("nya-n")
 	nyanHello.Nyan()
+
+	resp, err := http.Get("https://google.com/")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	f.Println(string(byteArray))
+	defer resp.Body.Close()
 }
