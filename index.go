8ab0391dfa66cbd05780f2b49e77f30559e6eb40
rchaser53 <tayoshizawa29@gmail.com>
2017-12-02 14:02:09 +0900

    try to call github api

--- index.go
+++ index.go
@@ -14,6 +14,7 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/hoge", serveTemplate)
+	http.HandleFunc("/git", serveGitRequest)
 
 	http.ListenAndServe(":3000", nil)
 }
@@ -38,8 +39,14 @@ func serveTemplate(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, indexFile)
 }
 
-func tryGet() string {
-	resp, err := http.Get("https://google.com/")
+func serveGitRequest(w http.ResponseWriter, r *http.Request) {
+	jsonStr := tryGet("https://api.github.com/repos/rchaser53/vue-table-playground/commits")
+	w.Header().Set("Content-Type", "application/json")
+	f.Fprint(w, jsonStr)
+}
+
+func tryGet(url string) string {
+	resp, err := http.Get(url)
 
 	if err != nil {
 		log.Fatal(err)
@@ -47,7 +54,7 @@ func tryGet() string {
 
 	byteArray, _ := ioutil.ReadAll(resp.Body)
 	bodyStr := string(byteArray)
-	f.Println(bodyStr)
+	// f.Println(bodyStr)
 	defer resp.Body.Close()
 
 	return bodyStr
