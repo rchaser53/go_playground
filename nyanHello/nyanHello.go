db6d25196fe8a936e12894547205f379b2d6a6e6
rchaser53 <tayoshizawa29@gmail.com>
2017-12-02 17:35:23 +0900

    try to read and copy

--- nyanHello/nyanHello.go
+++ nyanHello/nyanHello.go
@@ -1,7 +1,33 @@
 package nyanHello
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"strings"
+)
 
 func Nyan() {
 	fmt.Printf("nyan\n")
+}
+
+func TryReadAndCopy() {
+	aga, err := ioutil.ReadFile(`Makefile`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := strings.NewReader(string(aga))
+
+	var buf1, buf2 bytes.Buffer
+	w := io.MultiWriter(&buf1, &buf2)
+
+	if _, err := io.Copy(w, r); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(buf1.String())
+	fmt.Print(buf2.String())
 }
